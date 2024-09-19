8e8a2b79d9f61c946aed3de9f0d68461543a5123
khanhpn <phamkhanh90@gmail.com>
2024-09-19 20:56:17 +0700

    app android

--- app/src/main/java/com/khanhpn/firstapp/MainActivity.kt
+++ app/src/main/java/com/khanhpn/firstapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.khanhpn.firstapp.ui.theme.FirstAppTheme
 
@@ -38,10 +41,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Greeting(name = "Happy")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     FirstAppTheme {
-        Greeting("Android")
+        GreetingImage(message = "Hello", from = "Khanhpn")
     }
 }
